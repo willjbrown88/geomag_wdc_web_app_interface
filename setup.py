ab3715dc4344a572435c72e9ef6bb18ae50def1c
lbillingham <laurence.billingham@gmail.com>
2017-02-24 15:59:13 +0000

    trying to wire in travis

--- setup.py
+++ setup.py
@@ -2,6 +2,10 @@
 
 from setuptools import setup, find_packages
 
+requirements = [i.strip() for i in open("requirements.txt").readlines()]
+dev_requirements = [
+    i.strip() for i in open("requirements_dev.txt").readlines()
+]
 setup(
     name="geomag-webapp-interface",
     version="0.1.0",
@@ -9,4 +13,5 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
+    install_requires=requirements
 )
