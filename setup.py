d8e9b23d976f905bdabd2c3b3ce6a1e02b40fe82
Will Brown <wb@bgs.ac.uk>
2018-04-02 20:25:49 +0100

    Fix version number

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ dev_requirements = [
 ]
 setup(
     name="geomag-wdc-web-app-interface",
-    version="1.0.2",
+    version="1.0.3",
     description="Retrieve geomagnetic observatory data via WDC and INTERMAGNET web services",
     author="L. Billingham",
     maintainer="W. Brown",
@@ -16,7 +16,7 @@ setup(
     license="MIT",
     keywords=["geomagnetism", "geomagnetic observatory data", "wdc", "intermagnet"],
     url="https://github.com/willjbrown88/geomag_wdc_web_app_interface",
-    download_url="https://github.com/willjbrown88/geomag_wdc_web_app_interface/archive/1.0.2.tar.gz",
+    download_url="https://github.com/willjbrown88/geomag_wdc_web_app_interface/archive/1.0.3.tar.gz",
     
     python_requires=">=3.4",
     
