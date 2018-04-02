0a563f8988abc7e59ebbb8a4d59a790333b6fdfc
Will Brown <wb@bgs.ac.uk>
2018-04-02 20:19:05 +0100

    Adding install requires to setup.py for PyPI

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     license="MIT",
     keywords=["geomagnetism", "geomagnetic observatory data", "wdc", "intermagnet"],
     url="https://github.com/willjbrown88/geomag_wdc_web_app_interface",
-    download_url="https://github.com/willjbrown88/geomag_wdc_web_app_interface/archive/1.0.1.tar.gz",
+    download_url="https://github.com/willjbrown88/geomag_wdc_web_app_interface/archive/1.0.2.tar.gz",
     
     python_requires=">=3.4",
     
@@ -24,6 +24,14 @@ setup(
     include_package_data=True,
     package_data={'': ["*.ini"]},
     zip_safe=False,
-    install_requires=requirements,
-    extras_require={'develop': dev_requirements}
+    install_requires=['requests==2.12.4',
+                      'setuptools==27.2.0',
+                      'six==1.10.0',
+                      'sphinx==1.5.1'],
+    extras_require={'develop': ['ipython==5.1.0',
+                                'flake8==3.3.0',
+                                'pylint==1.6.4',
+                                'pytest==3.0.5',
+                                'pytest-cov==2.3.1',
+                                'sphinx==1.5.1']},
 )
